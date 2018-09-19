c055c64e5e704dced66eefe9d6d1703b710c75c8
Rupesh Jaiswal <RupeshJ@citiustech.com>
2018-09-19 19:06:16 +0530

    merged others changes

--- src/components/SuccessList/SuccessList.js
+++ src/components/SuccessList/SuccessList.js
@@ -16,23 +16,23 @@ class SuccessListTable extends Component {
     this.setState({ isLoading: true, parsedData});
   }
   render() {
-
+    const columnFilter = { type: 'TextFilter', delay: 1000 };
     return (
       <div>
         {
          this.state.isLoading &&
           <BootstrapTable data={ this.state.parsedData } striped={true} hover={true} pagination={ false }>
             <TableHeaderColumn dataField='Id' isKey dataAlign="center"
-              filter={ { type: 'TextFilter', delay: 1000 } }  >ID</TableHeaderColumn>
-            <TableHeaderColumn dataField='Name' filter={ { type: 'TextFilter', delay: 1000 } } 
+              filter={ columnFilter }  >ID</TableHeaderColumn>
+            <TableHeaderColumn dataField='Name' filter={ columnFilter } 
               dataAlign="center" >Name</TableHeaderColumn>
-            <TableHeaderColumn dataField='City' filter={ { type: 'TextFilter', delay: 1000 } }  
+            <TableHeaderColumn dataField='City' filter={ columnFilter }  
               dataAlign="center" >City</TableHeaderColumn>
-            <TableHeaderColumn dataField='Area' filter={ { type: 'TextFilter', delay: 1000 } }  
+            <TableHeaderColumn dataField='Area' filter={ columnFilter }  
               dataAlign="center" >Area</TableHeaderColumn>
-            <TableHeaderColumn dataField='Skill' filter={ { type: 'TextFilter', delay: 1000 } }  
+            <TableHeaderColumn dataField='Skill' filter={ columnFilter }  
               dataAlign="center" >Skill</TableHeaderColumn>
-            <TableHeaderColumn dataField='Year' filter={ { type: 'TextFilter', delay: 1000 } }  
+            <TableHeaderColumn dataField='Year' filter={ columnFilter }  
               dataAlign="center" >Year</TableHeaderColumn>
          </BootstrapTable>
         }
