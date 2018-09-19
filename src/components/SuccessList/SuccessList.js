2bb320ca50127ddeee47eb498ef9ade368385028
Engr.Sunday <asmelitus@gmail.com>
2018-09-19 22:14:17 +0100

    added to pagination at the top

--- src/components/SuccessList/SuccessList.js
+++ src/components/SuccessList/SuccessList.js
@@ -17,11 +17,22 @@ class SuccessListTable extends Component {
   }
   render() {
     const columnFilter = { type: 'TextFilter', delay: 1000 };
+    const options = {
+      page: 2, 
+      sizePerPage: 80,  
+      pageStartIndex: 0, 
+      paginationSize: 4,  
+      prePage: 'Prev', 
+      nextPage: 'Next',
+      firstPage: 'First', 
+      lastPage: 'Last',
+      paginationPosition: 'top'
+    };
     return (
       <div>
         {
          this.state.isLoading &&
-          <BootstrapTable data={ this.state.parsedData } striped={true} hover={true} pagination={ false }>
+          <BootstrapTable data={ this.state.parsedData } options= {options}striped={true} hover={true} pagination={ true }>
             <TableHeaderColumn dataField='Id' isKey dataAlign="center"
               filter={ columnFilter }  >ID</TableHeaderColumn>
             <TableHeaderColumn dataField='Name' filter={ columnFilter } 
